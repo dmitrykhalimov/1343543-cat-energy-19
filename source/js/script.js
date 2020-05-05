6414cd402ae3d56b2c870673847ad88cc196b4de
Bluesman <dmitry.khalimov@gmail.com>
2020-05-05 15:53:31 +0300

    Доступно отображение карты без JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,10 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle-menu');
-var pageMap = document.querySelector('.page-footer__map');
 
 var openedClass = 'main-nav--opened';
 var closedClass = 'main-nav--closed';
 
 navMain.classList.remove('main-nav--nojs');
-pageMap.classList.remove('page-footer__map--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains(closedClass)) {
